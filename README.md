16da57bb6b2efe60d229d0481979085efe33a1ac
jakariamasum <jmphy698@gmail.com>
2024-02-06 23:12:54 +0600

    multiple work done

---
+++ README.md
@@ -0,0 +1,9 @@
+# E Commerce Website - Cholo-Bazar
+---
+## Admin Login Credentials--
+* Email: cholobazar@admin.com
+* Password: 123456
+
+
+## Live website link--
+[Live Site](https://cholo-bazar-e-commerce.web.app/)
---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,39 @@
+// // Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA1sMwYEQZyIyLOAb3npfCBx38Ac2G_L3k",
+//   authDomain: "rokomari-clone-d0581.firebaseapp.com",
+//   projectId: "rokomari-clone-d0581",
+//   storageBucket: "rokomari-clone-d0581.appspot.com",
+//   messagingSenderId: "690331773295",
+//   appId: "1:690331773295:web:8d6055989ece06afc5609a"
+// };
+
+// // Initialize Firebase
+// const app = firebase.initializeApp(firebaseConfig);
+
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAz4b_14JZnL6JIKesild6WbFgPdAYRiTE",
+  authDomain: "cholobazar-ccb89.firebaseapp.com",
+  projectId: "cholobazar-ccb89",
+  storageBucket: "cholobazar-ccb89.appspot.com",
+  messagingSenderId: "190730543027",
+  appId: "1:190730543027:web:386bfa162beb63852775c6"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+export default app;
